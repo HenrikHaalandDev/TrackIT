62f0155cfe2294c156115fae9d2ac818bd918096
HenrikHaalandDev <henrikbibow@icloud.com>
2025-01-09 13:03:05 +0100

    starting template

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import os
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
-
-
+    if request.method == 'POST':
+        login_content = request.form['content']
+    else:
+        return render_template('home.html') 
